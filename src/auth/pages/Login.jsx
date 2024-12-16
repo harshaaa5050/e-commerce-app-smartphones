c132abf743ed42c41b935b93064c83e6dc70b1c5
Harsha <harshaaa0505@gmail.com>
2024-12-16 14:11:08 +0530

    Login and Signup page added

---
+++ src/auth/pages/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../../styles/style.css'
+import icon from '../../../public/assets/icon.png'
+
+
+const Login = () => {
+    return (
+        <div className='h-screen flex items-center justify-center'>
+            <div className='bg-transparent/25 w-1/4 h-[530px] rounded-2xl flex flex-col items-center justify-between'>
+                <div className='bg-transparent flex flex-col items-center w-full'>
+                    <img src={icon} alt="logo" className='w-16 mt-10' />
+                    <form className='flex flex-col justify-center gap-4 p-10 font-medium w-full'>
+                        <input type="text" placeholder="Enter your username" className='border border-black outline-black rounded-lg h-10 p-4' />
+                        <input type="password" placeholder="Enter password" className='border border-black outline-black rounded-lg h-10 p-4' />
+
+
+                        <input type="submit" value="Login" className='border bg-blue-300 h-11 rounded-lg outline-black border-black mt-5 cursor-pointer hover:bg-blue-400' />
+
+                    </form>
+                </div>
+                <div className='bg-transparent w-full h-12 flex font-medium rounded-2xl'>
+                    <NavLink className='h-full w-full rounded-2xl flex items-center justify-center cursor-default'>Login</NavLink>
+                    <NavLink to={'/signup'} className='h-full w-full bg-transparent/25 rounded-br-2xl shadow-inner flex items-center justify-center'>Sign Up</NavLink>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/auth/pages/Signup.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../../styles/style.css'
+import icon from '../../../public/assets/icon.png'
+
+const Signup = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <div className='bg-transparent/25 w-1/4 h-[530px] rounded-2xl flex flex-col items-center justify-between'>
+        <div className='bg-transparent flex flex-col items-center w-full'>
+
+          <img src={icon} alt="logo" className='w-16 mt-10' />
+          <form className='flex flex-col justify-center gap-4 p-10 font-medium w-full'>
+            <input type="email" placeholder="Enter email" className='border border-black outline-black rounded-lg h-10 p-4' />
+            <input type="text" placeholder="Enter username" className='border border-black outline-black rounded-lg h-10 p-4' />
+            <input type="password" placeholder="Enter password" className='border border-black outline-black rounded-lg h-10 p-4' />
+
+
+            <input type="submit" value="Sign Up" className='border bg-blue-300 h-11 rounded-lg outline-black border-black mt-5 cursor-pointer hover:bg-blue-400' />
+          </form>
+
+        </div>
+
+        <div className='bg-transparent w-full h-12 flex font-medium rounded-2xl'>
+          <NavLink to={'/login'} className='h-full w-full bg-transparent/25 rounded-bl-2xl shadow-inner flex items-center justify-center'>Login</NavLink>
+          <NavLink className='h-full w-full rounded-2xl flex items-center justify-center cursor-default'>Sign Up</NavLink>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Signup
