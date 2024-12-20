416d91aae0cf58c036355de8fd60b4251fa99176
Harsha <harshaaa0505@gmail.com>
2024-12-20 11:08:59 +0530

    wrapped an authentication provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './auth/pages/Login'
 import Signup from './auth/pages/Signup'
-import UserNavbar from './components/UserNavbar'
 import Cart from './user/pages/Cart'
 import Orders from './user/pages/Orders'
 import AdminNavbar from './components/AdminNavbar'
@@ -10,23 +9,35 @@ import Home from './user/pages/Home'
 import ProductDetails from './user/pages/ProductDetails'
 import Footer from './components/Footer'
 import Checkout from './user/pages/Checkout'
+import ManageProducts from './admin/pages/ManageProducts'
+import Users from './admin/pages/Users'
+import ViewOrders from './admin/pages/ViewOrders'
+import Dashboard from './admin/pages/Dashboard'
+import AddProducts from './admin/pages/AddProducts'
+import { AuthProvider } from './contexts/AuthContexts'
 
 const App = () => {
   return (
-    <Router>
+
+      <AuthProvider>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/usernavbar" element={<UserNavbar/>} />
-          <Route path="/cart" element={<Cart/>} />
-          <Route path="/checkout" element={<Checkout/>} />
-          <Route path="/orders" element={<Orders/>} />
-          <Route path="/adminnavbar" element={<AdminNavbar/>} />
-          <Route path="/products/:id" element={<ProductDetails/>} />
-          <Route path="/footer" element={<Footer/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/footer" element={<Footer />} />
+
+          <Route path="/adminnavbar" element={<AdminNavbar />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/admin/manageproducts" element={<ManageProducts />} />
+          <Route path="/admin/users" element={<Users />} />
+          <Route path="/admin/vieworders" element={<ViewOrders />} />
+          <Route path="/admin/addproducts" element={<AddProducts />} />
         </Routes>
-      </Router>
+      </AuthProvider>
   )
 }
 
