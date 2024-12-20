a2b773af3f717a858d7d4acc0dd34a52155f08e9
Harsha <harshaaa0505@gmail.com>
2024-12-20 16:23:27 +0530

    separate navbar components for logged in and for not logged in users

--- src/components/UserNavbar.jsx
+++ src/components/UserNavbar.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import icon from '../../public/assets/icon.png'
 import cart_icon from '../../public/assets/cart_icon.png'
 import search from '../../public/assets/search.png'
 import account from '../../public/assets/account.png'
 import orders from '../../public/assets/orders.png'
+import { AuthContext } from '../contexts/AuthContexts'
 
 const UserNavbar = () => {
 
     const navigate = useNavigate();
+    const {user, logout} = useContext(AuthContext);
 
     return (
         <nav className='flex-col shadow-md p-2 w-full fixed bg-white'>
@@ -33,16 +35,18 @@ const UserNavbar = () => {
                         <h2 className='font-semibold sm:flex hidden'>Cart</h2>
                     </NavLink>
 
-                    {/* <div className='relative group'>
+                    {user ? 
 
-                        account symbol and name
+                    <div className='relative group'>
+                       
                         <div className='flex items-center justify-between cursor-pointer'>
                             <img src={account} alt="account" className='w-7' />
                             <h2 className='font-semibold sm:flex hidden'>Username</h2>
                         </div>
 
                         <div className='absolute opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 right-0 bg-white border border-gray-300 rounded-lg shadow-lg pt-2 w-32'>
-                            orders icon and name
+                            
+
                             <NavLink to='/orders' className='flex items-center justify-center gap-1 text-gray-700 hover:bg-gray-100 px-4 py-2'>
                                 <img src={orders} alt="orders" className='w-7' />
                                 <h2 className='font-semibold'>Orders</h2>
@@ -50,14 +54,14 @@ const UserNavbar = () => {
 
 
 
-                            logout button
+                            
                             <div className='flex items-center justify-center px-4 py-2'>
-                                <NavLink to="/login" className='w-32 h-10 flex items-center justify-center font-medium rounded-2xl bg-pink-300 hover:bg-pink-400'>Logout</NavLink>
+                                <button onClick={logout} className='w-32 h-10 flex items-center justify-center font-medium rounded-2xl bg-pink-300 hover:bg-pink-400'>Logout</button>
                             </div>
                         </div>
-                    </div> */}
-
-                    <NavLink to="/login" className='w-32 h-10 flex items-center justify-center font-medium rounded-2xl bg-pink-300 hover:bg-pink-400'>Login/Signup</NavLink>
+                    </div>
+                    :
+                    <NavLink to="/login" className='w-32 h-10 flex items-center justify-center font-medium rounded-2xl bg-pink-300 hover:bg-pink-400'>Login/Signup</NavLink>}
                 </div>
 
             </div>
