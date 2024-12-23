8e935ce341398c4d36efd182d346a5da99ccfc93
Harsha <harshaaa0505@gmail.com>
2024-12-23 16:31:05 +0530

    Adding and removing data from local storage

--- src/contexts/AuthContexts.jsx
+++ src/contexts/AuthContexts.jsx
@@ -1,91 +1,97 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { addUser, checkUser, checkUsername, fetchUsers } from '../api/userApi';
-
+import { addUser, checkUsername, checkUser, fetchUsers } from '../api/userApi'
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const navigate = useNavigate();
-    const [user, setUser] = useState();
+    const [user, setUser] = useState(null);
 
-    useEffect(() =>{
-        const storedUser = localStorage.getItem("user");
-        const storedAdmin = localStorage.getItem("admin");
-        if (storedUser){
-            setUser(JSON.parse(storedUser));
-        } else if (storedAdmin){
-            setUser(JSON.parse(storedAdmin));
-        }
-    },[])
+    // useEffect(() => {
+    //     const storedUser = localStorage.getItem("user");
+    //     const storedAdmin = localStorage.getItem("admin");
+    //     if (storedUser) {
+    //         setUser({ ...storedUser, role: 'user' });
+    //     } else if (storedAdmin) {
+    //         setUser({ ...storedAdmin, role: 'admin' });
+    //     }
+    // }, []);
 
-    const userSignup = async (newUser) => {
+    const userSignup = async (userData) => {
         try {
-            const emailTaken = await checkUser(newUser.email);
-            if (emailTaken){
-                throw new Error("Account already exists with this email");
+            const emailExist = await checkUser(userData.email);
+            if (emailExist) {
+                throw new Error("An account already exists in this email");
             }
 
-            const usernameTaken = await checkUsername(newUser.username);
-            if (usernameTaken){
-                throw new Error("Username already taken");
+            const usernameExist = await checkUsername(userData.username);
+            if (usernameExist) {
+                throw new Error("Username already in use.");
             }
+
+            const newUser = {
+                ...userData,
+                "role": "user",
+                "block": false,
+                "cart": []
+            };
 
             const response = await addUser(newUser);
+            localStorage.setItem("user", response.data.id);
+            localStorage.setItem("username", response.data.username);
+            localStorage.setItem("role", "user");
+            setUser(newUser);
 
-            localStorage.setItem(
-                "user",
-                JSON.stringify({
-                    userId : response.data.id,
-                    username : response.data.username
-                })
-            );
-
-            setUser(response.data);
             setTimeout(() => {
                 navigate("/");
-            },2000);
-
-        } catch(error){
-            throw error;
-        }
-    }
-
-    const userLogin = async ({username, password}) =>{
-        try {
-            const {data: users} = await fetchUsers();
-
-            const findUser = users.find((user) => user.username === username && user.password === password);
-
-            if(!findUser){
-                throw new Error('Invalid username or password');
-            }
-            if(findUser.block){
-                throw new Error('Your account is blocked');
-            }
-
-            localStorage.setItem(
-                findUser.role,
-                JSON.stringify({
-                    userId : findUser.id,
-                    username : findUser.username
-                })
-            );
-
-            setUser(findUser);
-            setTimeout(() => {
-                navigate("/");
-            },2000);
+            }, 1000);
 
         } catch (error) {
             throw error;
         }
     }
 
+    const userLogin = async (userData) => {
+        try {
+            const { data: users } = await fetchUsers();
 
+            const foundedUser = users.find((user) =>
+                user.username === userData.username && user.password === userData.password
+            );
+
+            if (!foundedUser) {
+                throw new Error("Invalid Username or Password");
+            }
+
+            if (foundedUser.block) {
+                throw new Error("Your account is blocked");
+            }
+
+            localStorage.setItem("user", foundedUser.id);
+            localStorage.setItem("username", foundedUser.username);
+            localStorage.setItem("role", foundedUser.role);
+
+            setUser(foundedUser);
+            setTimeout(() => {
+                navigate("/");
+            }, 1000);
+
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    const logout = () => {
+        localStorage.removeItem("user");
+        localStorage.removeItem("username");
+        localStorage.removeItem("role");
+        setUser(null);
+        navigate('/');
+    };
 
     return (
-        <AuthContext.Provider value={{ user, userSignup, userLogin }}>
+        <AuthContext.Provider value={{ user, userSignup, userLogin, logout }} >
             {children}
         </AuthContext.Provider>
-    );
-}
+    )
+};
